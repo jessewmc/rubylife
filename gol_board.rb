1e1efca43e78997a2231766802a58701a3e956f5
MC <jessewmc@gmail.com>
2011-12-19 20:39:13 -0700

    Added size instance to game board

--- gol_board.rb
+++ gol_board.rb
@@ -1,7 +1,8 @@
 class GameOfLifeBoard
 
   def initialize(size=50)
-    @cells = Array.new(size) do |i|
+    @size = size
+    @cells = Array.new(@size) do |i|
       Array.new(size, Cell.new)
     end
   end

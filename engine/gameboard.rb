d417d04f09debf5d21347e6a22f788cb7f300734
MC <jessewmc@gmail.com>
2011-12-26 09:58:04 -0700

    Reorganization

--- engine/gameboard.rb
+++ engine/gameboard.rb
@@ -1,7 +1,7 @@
 #todo: make this gameboard, remove next_gen
 #load/dump should be here?
 
-class GameOfLifeBoard
+class GameBoard
 
   LIVE_CELL = "x"
   DEAD_CELL = "."
@@ -45,38 +45,6 @@ class GameOfLifeBoard
 
   end
 
-  #returns a new GameOfLifeBoard object representing the next
-  #generation as calculated from this generation
-  def next_gen
-
-    next_board = new_board(@size)
-
-    each_index do |x, y|
-      neighbours=0
-      (x-1).upto(x+1) do |i|
-        (y-1).upto(y+1) do |j|
-
-          if @cells[i].nil? then next end
-
-          if @cells[i][j].nil? then next end
-
-          if @cells[i][j].alive? then neighbours +=1 end
-
-        end
-      end
-
-      if @cells[x][y].alive?
-        neighbours-=1
-        if (2..3).member? neighbours
-          next_board[x][y].alive!
-        end
-      elsif neighbours == 3
-        next_board[x][y].alive!
-      end
-    end
-
-    return GameOfLifeBoard.new(@size, next_board)
-  end
 
   private
     

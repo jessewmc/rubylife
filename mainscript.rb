494cf7c75b115ba0e3e595797d0a65485082bf5a
MC <jessewmc@gmail.com>
2011-12-26 11:02:24 -0700

    Basic functionality added

--- mainscript.rb
+++ mainscript.rb
@@ -1,8 +1,21 @@
 #!/usr/bin/ruby
-require 'gol_cell.rb'
-require 'gol_board.rb'
-require 'gameoflife.rb'
+require 'engine/cellularautomata.rb'
 
 if ARGV.size == 0
-  print `cat README`
+  #exits program and prints string argument. `...` is the string containing
+  #the result of the commandline command
+  abort `cat README`
 end
+
+game = CellularAutomata.new
+game.load_board "#{ARGV[0]}"
+print game
+
+if ARGV.size == 2
+  0.upto ARGV[1].to_i do
+    sleep 0.6
+    game.next_gen!
+    print game
+  end
+end
+

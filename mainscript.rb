27023b336b3bed1ffcbcf380e50f9067e253c3bb
MC <jessewmc@gmail.com>
2011-12-27 12:58:11 -0700

    Mainscript command line option parsing added

--- mainscript.rb
+++ mainscript.rb
@@ -1,25 +1,50 @@
 #!/usr/bin/ruby
 require 'engine/cellularautomata.rb'
+require 'optparse'
 
-if ARGV.size == 0
-  #exits program and prints string argument. `...` is the string containing
-  #the result of the commandline command
-  abort `cat usage`
-end
+#Simple script using Ruby's built in command line option parser.
 
-game = CellularAutomata.new
-game.load_board "#{ARGV[0]}"
-print game
+options = {}
 
-if ARGV.size >= 2
-  1.upto ARGV[1].to_i do
-    sleep 0.8
-    game.next_gen!
-    print "\n"
-    print game
+
+optparse = OptionParser.new do |opts|
+  
+  opts.banner = `cat usage` 
+
+  options[:generations] = 0
+  opts.on( '-g', '--generations N', Integer, 'Number of generations to be computed' ) do |g|
+    options[:generations] = g
+  end
+
+  opts.on( '-o', '--output [FILE]', String, 'Output file to which final generation is written' ) do |o|
+    options[:output] = o
+  end
+
+  options[:delay] = 0.8
+  opts.on( '-d', '--delay N', Float, 'Delay between printed generations in seconds -- try 0 < N < 1' ) do |d|
+    options[:delay] = d
+  end
+
+  opts.on( '-i', '--input [FILE]', String, 'Input .cel file' ) do |i|
+    options[:input] = i
   end
 end
 
-if ARGV.size >= 3
-  game.write_board(ARGV[2])
+optparse.parse!
+
+abort `cat usage` unless options[:input]
+
+game = CellularAutomata.new
+game.load_board options[:input]
+print game
+
+1.upto options[:generations] do
+    sleep options[:delay]
+    game.next_gen!
+    print "\n"
+    print game
+end
+
+if options[:output]
+  game.write_board(options[:output])
 end
